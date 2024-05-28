68a3bd12149403b8e0fb26af80a84c3b5a028521
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-05-28 10:26:08 +0200

    Update dag.py

---
+++ airflow/dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'hadoop_data_pipeline',
+    default_args=default_args,
+    description='A simple data pipeline for Hadoop',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id='start_hadoop_job',
+    bash_command='hadoop jar my_hadoop_job.jar',
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id='upload_to_s3',
+    bash_command='aws s3 cp /path/to/data s3://healthcare-patient-data/',
+    dag=dag,
+)
+
+t1 >> t2
