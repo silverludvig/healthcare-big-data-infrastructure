202ef825075ab469e25e88cb4eb3b4aee526c1e9
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-24 19:47:33 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,10 @@
+# Define a variable for the AWS region
 variable "region" {
   description = "The AWS region to deploy resources in"
   default     = "eu-north-1"
 }
 
+# Use the region variable in the provider block
+provider "aws" {
+  region = var.region
+}
