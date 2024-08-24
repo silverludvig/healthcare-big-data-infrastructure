7d036cbaf8ff9f2c9e73d404cebdc3e03f05a014
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-24 19:40:40 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,3 +97,23 @@ resource "aws_iam_role_policy_attachment" "emr_service_policy_attachment" {
   role       = aws_iam_role.emr_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
 }
+
+
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+}
+
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_rt.id
+}
