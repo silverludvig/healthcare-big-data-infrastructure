6d7ab383494f4a994e82f613f0b7554d188990b0
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-24 18:49:43 +0200

    Update outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+# Output the RDS endpoint
+output "rds_endpoint" {
+  description = "The endpoint of the Amazon RDS instance"
+  value       = aws_rds_instance.healthcare_db.endpoint
+}
+
+# Output the EMR cluster ID
+output "emr_cluster_id" {
+  description = "The ID of the Amazon EMR cluster"
+  value       = aws_emr_cluster.emr_cluster.id
+}
