4ef7cdfb7889eb3973df8b2279a1b90d66a295fc
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-31 16:00:26 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,49 @@
 # Define a variable for the AWS region
 variable "region" {
   description = "The AWS region to deploy resources in"
+  type        = string
   default     = "eu-north-1"
 }
 
-# Use the region variable in the provider block
-provider "aws" {
-  region = var.region
+
+# Define a variable for the environment (e.g., dev, staging, prod)
+variable "environment" {
+  description = "The environment for deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
 }
+
+# Define a variable for the instance type for the EMR cluster
+variable "emr_instance_type" {
+  description = "The instance type to use for the EMR cluster nodes"
+  type        = string
+  default     = "m5.xlarge"
+}
+
+# Define a variable for the RDS instance class
+variable "rds_instance_class" {
+  description = "The instance class to use for the RDS instance"
+  type        = string
+  default     = "db.t2.micro"
+}
+
+# Define a variable for the RDS allocated storage
+variable "rds_allocated_storage" {
+  description = "The allocated storage in GB for the RDS instance"
+  type        = number
+  default     = 20
+}
+
+# Define a variable for the RDS username
+variable "rds_username" {
+  description = "The master username for the RDS instance"
+  type        = string
+  default     = "admin"
+}
+
+# Define a variable for the RDS password (Sensitive)
+variable "rds_password" {
+  description = "The master password for the RDS instance"
+  type        = string
+  sensitive   = true
+  default     = "RDS-Password#123"  
