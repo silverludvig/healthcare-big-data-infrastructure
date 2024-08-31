5fc7e6c5418678980f78dba4adb0b420fc29c87d
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-31 16:04:12 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,16 @@
 provider "aws" {
-  region = "eu-north-1"  
-  version = "~> 4.0" 
+  region  = "eu-north-1"
+  version = "~> 4.0"
+
 }
 
 terraform {
   required_version = ">= 1.5.0"
-}
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
