708407daa1605b2e5f2b4cc21d40d6e5c9447b93
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-31 16:04:52 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,33 @@ output "emr_cluster_id" {
   description = "The ID of the Amazon EMR cluster"
   value       = aws_emr_cluster.emr_cluster.id
 }
+
+# Output the Jenkins server public IP
+output "jenkins_server_public_ip" {
+  description = "The public IP address of the Jenkins server"
+  value       = aws_instance.jenkins_server.public_ip
+}
+
+# Output the Jenkins server URL
+output "jenkins_server_url" {
+  description = "The URL to access the Jenkins server"
+  value       = format("http://%s:8080", aws_instance.jenkins_server.public_ip)
+}
+
+# Output the CloudWatch Log Group ARN for RDS
+output "rds_log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for RDS"
+  value       = aws_cloudwatch_log_group.rds_log_group.arn
+}
+
+# Output the CloudWatch Log Group ARN for EMR
+output "emr_log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for EMR"
+  value       = aws_cloudwatch_log_group.emr_log_group.arn
+}
+
+# Output the CloudWatch Log Group ARN for Jenkins
+output "jenkins_log_group_arn" {
+  description = "The ARN of the CloudWatch Log Group for Jenkins"
+  value       = aws_cloudwatch_log_group.jenkins_log_group.arn
+}
