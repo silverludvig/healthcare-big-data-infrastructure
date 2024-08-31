9048fdf511e3400f7035d02052c66bb122ab1f31
Quanle Dong <94353353+silverludvig@users.noreply.github.com>
2024-08-31 18:46:01 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-north-1a"  # Adjust the availability zone as needed
+  availability_zone = "eu-north-1a" 
 }
 
 # Define a Security Group for the EMR cluster and other resources
